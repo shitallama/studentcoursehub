c2f4a7e5bc1ea3d44d055e753afd37545ad92f97
Shital Lama Tamang <59154@edu.nielsbrock.dk>
2026-03-12 17:19:38 +0100

    Add footer with copyright and GitHub link

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>&copy; 2026 Student Web Course. All rights reserved.</p>
+    <p><a href="https://github.com/shitallama/studentcoursehub">GitHub Repository</a></p>
+</footer>
+</body>
+</html>
